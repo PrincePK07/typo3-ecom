fec8df8f0127d76f6d6d3368bba113eb99934b35
Prince PK <prince.pk@pitsolutions.com>
2023-11-08 14:31:32 +0530

    Content-element

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,3 +1,13 @@
 #
 # Add SQL definition of database tables
 #
+CREATE TABLE tt_content (
+
+    -- banner element
+    bannerimage int(11) unsigned DEFAULT '0' NOT NULL,
+    bannerheading varchar(255) DEFAULT '' NOT NULL ,
+    bannersubheading tinytext DEFAULT '' NOT NULL ,
+    bannerbutton varchar(11) DEFAULT '' NOT NULL ,
+
+);
+
